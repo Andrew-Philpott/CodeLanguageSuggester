22b2051df3c4638c5b2e6086dda793e1b8f236a0
Andrew Philpott <andrewphilpott92@gmail.com>
2019-09-20 22:51:28 -0700

    Change value of year-developed radio series from 0 and 1 to before and after. Create javascript objects with property value pairs representing the different programming languages and store them in an array. Change the return values of check input functions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,26 @@
 $(document).ready(function() {
+  var javaScript = {
+    languageUse: "web",
+    paradigms: "less",
+    letter: "j",
+    animalType: "cat",
+    developed: "before"
+  }
+  var cSharp = {
+    languageUse: "application",
+    paradigms: "more",
+    letter: "c",
+    animalType: "cat",
+    developed: "after"
+  }
+  var python = {
+    languageUse: "data",
+    paradigms: "more",
+    letter: "p",
+    animalType: "dog",
+    developed: "before"
+  }
+  var programmingLanguagesArray = [javaScript, cSharp, python];
   $("#user-input-container form").submit(function(event) {
     var yearDevelopedInput = $("input:radio[name='year-developed']:checked").val();
     var languageUseInput = $("input:radio[name='language-use']:checked").val();
@@ -17,40 +39,45 @@ $(document).ready(function() {
     if(inputArg === undefined) {
       $("#yearDevelopedInputContainer p").remove();
       $("#yearDevelopedInputContainer").append("<p>Please select an option</p>");
+      return false;
     } else {
-      return "";
+      return true;
     }
   }
   function checkLanguageUseInput(inputArg) {
     if(inputArg === undefined) {
       $("#languageUseInputContainer p").remove();
       $("#languageUseInputContainer").append("<p>Please select an option</p>");
+      return false;
     } else {
-      return "";
+      return true;
     }
   }
   function checkParadigmAmountInput(inputArg) {
     if(inputArg === undefined) {
       $("#paradigmAmountInputContainer p").remove();
       $("#paradigmAmountInputContainer").append("<p>Please select an option</p>");
+      return false;
     } else {
-      return "";
+      return true;
     }
   }
   function checkBestLetterInput(inputArg) {
     if(inputArg === undefined) {
       $("#bestLetterInputContainer p").remove();
       $("#bestLetterInputContainer").append("<p>Please select an option</p>");
+      return false;
     } else {
-      return "";
+      return true;
     }
   }
   function checkBestAnimalInput(inputArg) {
     if(inputArg === undefined) {
       $("#bestAnimalInputContainer p").remove();
       $("#bestAnimalInputContainer").append("<p>Please select an option</p>");
+      return false;
     } else {
-      return "";
+      return true;
     }
   }
 });
