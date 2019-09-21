e76b69e90b561a3870c843bb2393643617da2438
Andrew Philpott <andrewphilpott92@gmail.com>
2019-09-21 10:31:24 -0700

    Fix name for paradigm question. Add python, csharp, and javascript images, change name of programming language data objects. Create variables to hold calculated scores of programming languages and create logic for showing suggested options.

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,58 @@
 $(document).ready(function() {
-  var javaScript = {
+  var javaScriptData = {
     languageUse: "web",
     paradigms: "less",
     letter: "j",
     animalType: "cat",
     developed: "before"
   }
-  var cSharp = {
+  var cSharpData = {
     languageUse: "application",
     paradigms: "more",
     letter: "c",
     animalType: "cat",
     developed: "after"
   }
-  var python = {
+  var pythonData = {
     languageUse: "data",
     paradigms: "more",
     letter: "p",
     animalType: "dog",
     developed: "before"
   }
-  var programmingLanguagesArray = [javaScript, cSharp, python];
+  var programmingLanguagesArray = [javaScriptData, cSharpData, pythonData];
   $("#user-input-container form").submit(function(event) {
     var languageUseInput = $("input:radio[name='language-use']:checked").val();
     var paradigmAmountInput = $("input:radio[name='paradigm-amount']:checked").val();
     var bestLetterInput = $("input:radio[name='best-letter']:checked").val();
     var bestAnimalInput = $("input:radio[name='animal-type']:checked").val();
     var yearDevelopedInput = $("input:radio[name='year-developed']:checked").val();
+    var javaScript = calculateProgrammingLanguageScore(languageUseInput, paradigmAmountInput, bestLetterInput, bestAnimalInput, yearDevelopedInput, javaScriptData).toString();
+    var cSharp = calculateProgrammingLanguageScore(languageUseInput, paradigmAmountInput, bestLetterInput, bestAnimalInput, yearDevelopedInput, cSharpData).toString();
+    var python = calculateProgrammingLanguageScore(languageUseInput, paradigmAmountInput, bestLetterInput, bestAnimalInput, yearDevelopedInput, pythonData).toString();
 
+    if (python > cSharp && python > javaScript) {
+      $("#python-hidden").show();
+    } else if (cSharp > python && cSharp > javaScript) {
+      $("#csharp-hidden").show();
+    } else if (javaScript > cSharp && javaScript > python) {
+      $("#javascript-hidden").show();
+    } else if (cSharp === javaScript === python) {
+      $("#python-hidden").show();
+      $("#javascript-hidden").show();
+      $("#csharp-hidden").show();
+    } else if (cSharp === javaScript) {
+      $("#javascript-hidden").show();
+      $("#csharp-hidden").show();
+    } else if (cSharp === python) {
+      $("#python-hidden").show();
+      $("#csharp-hidden").show();
+    } else if (javaScript === python) {
+      $("#python-hidden").show();
+      $("#javascript-hidden").show();
+    } else {
+
+    }
     event.preventDefault();
   });
   function calculateProgrammingLanguageScore(languageUseArg, paradigmAmountArg, bestLetterArg, bestAnimalArg, yearDevelopedArg, language) {
