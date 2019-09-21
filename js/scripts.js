d040e328e87a7e829c2fd6d3a8871eb08851d35b
Andrew Philpott <andrewphilpott92@gmail.com>
2019-09-20 22:31:38 -0700

    Create div for each question set. Add functions to check that a choice is selected for each radio series. Create variables to store user input from selected radio buttons.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,56 @@
 $(document).ready(function() {
   $("#user-input-container form").submit(function(event) {
-  
+    var yearDevelopedInput = $("input:radio[name='year-developed']:checked").val();
+    var languageUseInput = $("input:radio[name='language-use']:checked").val();
+    var paradigmAmountInput = $("input:radio[name='paradigm-amount']:checked").val();
+    var bestLetterInput = $("input:radio[name='best-letter']:checked").val();
+    var bestAnimalInput = $("input:radio[name='animal-type']:checked").val();
+    checkYearDevelopedInput(yearDevelopedInput);
+    checkLanguageUseInput(languageUseInput);
+    checkParadigmAmountInput(paradigmAmountInput);
+    checkBestLetterInput(bestLetterInput);
+    checkBestAnimalInput(bestAnimalInput);
+
+    event.preventDefault();
   });
+  function checkYearDevelopedInput(inputArg) {
+    if(inputArg === undefined) {
+      $("#yearDevelopedInputContainer p").remove();
+      $("#yearDevelopedInputContainer").append("<p>Please select an option</p>");
+    } else {
+      return "";
+    }
+  }
+  function checkLanguageUseInput(inputArg) {
+    if(inputArg === undefined) {
+      $("#languageUseInputContainer p").remove();
+      $("#languageUseInputContainer").append("<p>Please select an option</p>");
+    } else {
+      return "";
+    }
+  }
+  function checkParadigmAmountInput(inputArg) {
+    if(inputArg === undefined) {
+      $("#paradigmAmountInputContainer p").remove();
+      $("#paradigmAmountInputContainer").append("<p>Please select an option</p>");
+    } else {
+      return "";
+    }
+  }
+  function checkBestLetterInput(inputArg) {
+    if(inputArg === undefined) {
+      $("#bestLetterInputContainer p").remove();
+      $("#bestLetterInputContainer").append("<p>Please select an option</p>");
+    } else {
+      return "";
+    }
+  }
+  function checkBestAnimalInput(inputArg) {
+    if(inputArg === undefined) {
+      $("#bestAnimalInputContainer p").remove();
+      $("#bestAnimalInputContainer").append("<p>Please select an option</p>");
+    } else {
+      return "";
+    }
+  }
 });
